7b76d08c9ca5af1604d09a549ae297d3bb38b48c
Your Name <you@example.com>
2020-10-19 10:42:12 +0200

    Simple http server, blocking cpu, memory usage

--- polecenia.md
+++ polecenia.md
@@ -1,8 +1,15 @@
 
 # Git clone
-git clone https://bitbucket.org/ev45ive/node-micro-gsk.git
+<!-- git clone https://bitbucket.org/ev45ive/node-micro-gsk.git -->
+https://bitbucket.org/ev45ive/node-micro-gsk/src => (+) => Fork
 
-https://bitbucket.org/ev45ive/node-micro-gsk/src
-
-
+git clone https://bitbucket.org/<twoj user>/node-micro-gsk.git
+cd node-micro-gsk
 git remote add matt https://bitbucket.org/ev45ive/node-micro-gsk.git
+
+# Stash local changes, pull remote changes
+git stash -u
+git pull -t matt master 
+git pull
+
+
--- start/index.js
+++ start/index.js
@@ -3,12 +3,40 @@ const http = require('http')
 const hostname = '127.0.0.1'
 const port = process.env.PORT || 8080
 
+var i = 0;
+
 const server = http.createServer((req, res) => {
- res.statusCode = 200
- res.setHeader('Content-Type', 'text/plain')
- res.end('Hello World!\n')
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+
+  if (req.url.includes('args')) {
+    res.setHeader('Content-Type', 'text/json')
+    res.end(JSON.stringify(process.argv, 0, 2))
+
+  } else if (req.url.includes('counter')) {
+    res.end(String(i++))
+
+  } else if (req.url.includes('calculate')) {
+    /* DONT DO THIS - DONT BLOCK CPU */
+    console.log('sleep for 5sec')
+    now = Date.now()
+    while (now + 5000 > Date.now()) {/* CPU intensive task */ }
+    res.end(String('done!'))
+    
+  }else if (req.url.includes('bigfile')) {
+    
+    var bigfile = ''
+    for(let i = 0; i < 10e5; i++){
+      bigfile += 'Ala ma kota, ' + i + '\r\n';
+    }
+    res.end(bigfile)
+    
+  } else {
+    res.end('Hello World!\n')
+  }
+
 })
 
 server.listen(port, hostname, () => {
- console.log(`Server running at http://${hostname}:${port}/`)
+  console.log(`Server running at http://${hostname}:${port}/`)
 })
