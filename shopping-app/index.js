2cc42eb79a06641420293062b7eda6bee3755ce6
Your Name <you@example.com>
2020-10-19 16:36:21 +0200

    shopping-app

--- shopping-app/index.js
+++ shopping-app/index.js
@@ -1,8 +1,14 @@
+const express = require('express')
 
-
+const app = express()
 
 // List products + filter by name?
-
+app.get('')
 // Get product details
 
 // Get categories
+
+
+app.listen(process.env.PORT || 8080, process.env.HOST || 'localhost', () => {
+  console.log(``)
+})
