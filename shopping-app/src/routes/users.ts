08bcbead0cec3577237abac53f5a8001b043caa6
Your Name <you@example.com>
2020-10-20 11:28:44 +0200

    users service

--- shopping-app/src/routes/users.ts
+++ shopping-app/src/routes/users.ts
@@ -1,9 +1,15 @@
 import * as express from "express";
+import { getUsers } from "../services/users";
  
 export const usersRoutes = express.Router();
 
 // Get Users
-usersRoutes.get('/',(req,res)=>{})
+usersRoutes.get('/',(req,res)=>{
+  
+  getUsers().then(users => {
+    res.send(users)
+  })
+})
 
 // Get Current User
 usersRoutes.get('/me',(req,res)=>{})
---
+++ shopping-app/src/services/users.ts
@@ -0,0 +1,26 @@
+
+const usersData = [
+  {
+    id:123, name:'admin', password:'admin'
+  },
+  {
+    id:234, name:'user', password:'user'
+  },
+]
+
+
+export const getUsers = () => {
+  return usersData
+}
+
+export const getUserById = () => {}
+
+export const createUser = () => {}
+
+export const updateUser = () => {}
+
+export const signupUser = () => {}
+
+export const signinUser = () => {}
+
+export const signoutUser = () => {}
