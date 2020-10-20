55e678196e4d337bef39900e132cc28a6ea02b3b
Szymon Mackowiak <szymon.x.mackowiak@gsk.com>
2020-10-20 15:46:43 +0200

    payaments

--- shopping-app/src/services/payments.ts
+++ shopping-app/src/services/payments.ts
@@ -1,12 +1,38 @@
 import path from "path"
+import { getUserById } from "./users";
 
 let userAccount = {
   valid: true,
   money: 0
 }
 
+const payments = [];
+
 export const addMoney = (cash) => {
   userAccount.money += cash
   return ("dodano " + userAccount.money + "pln")
 }
 
+export const createPayment = (userID, productID, value) => {
+  let newPayment = {
+    userId: userID,
+    paymentID: Date.now().toString(),
+    product: productID,
+    orderValue: value,
+    status: 'new'
+  }
+  payments.push(newPayment)
+}
+
+export const getPaymentStatus = (paymentID) => {
+  let orderSelection = payments.filter(item => {
+    return item.paymentID === paymentID
+  });
+
+  return orderSelection[0].status;
+}
+
+export const changeStatus = (paymentID) => {
+  const index = payments.indexOf(element => element.paymentID = paymentID);
+  payments[index].status = 'pending'
+}
