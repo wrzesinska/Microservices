c9f9427cdca85a8afd8cdce972615c4bf1f9a13b
Julia Jedryczka <julia.x.jedryczka@gsk.com>
2020-10-20 15:18:44 +0200

    pages post

--- shopping-app/src/routes/pages.ts
+++ shopping-app/src/routes/pages.ts
@@ -9,3 +9,12 @@ pagesRoutes.get('/', async (req, res) => {
     const answer = pagesService.productArticles();
     res.send(answer)
 })
+
+//Get description of a given product
+pagesRoutes.post('/:productId', [], async (req, res) => {
+
+    const product_id = req.params['productId'];
+    const product = require(`../../data/products/${product_id}/product.json`)
+
+    res.status(200).send(product.description)
+})
