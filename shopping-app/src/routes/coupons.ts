3ab33b3bfc189eaf647a338a66e8a5d4ba8a05cf
Bartosz Bak <bartosz.x.bak@gsk.com>
2020-10-20 22:37:41 +0200

    add coupon

--- shopping-app/src/routes/coupons.ts
+++ shopping-app/src/routes/coupons.ts
@@ -1,7 +1,29 @@
 import * as express from 'express'
 import * as couponService from '../services/coupons'
+import { body, validationResult } from 'express-validator'
 
 export const couponRoutes = express.Router()
+
+// add new coupon
+
+// couponRoutes.get('/', async (req, res) => {
+//     const result = await couponService.getCouponById(req.params.id)
+//     res.json(result)
+//   })
+  
+// couponRoutes.post('/', [
+//     body('email').isEmail(),
+//   ], async (req, res) => {
+//     const createCoupon = req.body
+//     const newCoupon = await couponService.createCoupon(createCoupon)
+//     res.status(201).send({ ok: true, newCoupon})
+//   })
+  
+// couponRoutes.get('/:coupon-id', async (req, res) => {
+//     const coupon_id = req.params['coupon_id']
+  
+//     res.send(couponService.getCouponById(coupon_id))
+//   })
 
 couponRoutes.get('/', async (req, res) => {
     const coupon = couponService.getCoupons()
--- shopping-app/src/services/coupons.ts
+++ shopping-app/src/services/coupons.ts
@@ -1,16 +1,26 @@
 import path from "path"
+import {randomBytes} from 'crypto'
 
-const coupon1 = {
-	id : 15012,
-	discount : 0.2,
-  }
+const coupons = [ 
+    { 
+        id : 15012, discount : 0.2,
+    },
+    { 
+        id : 15045, discount : 0.5,
+    }
+] 
 
 export const getCoupons = () => {
-
-    return [coupon1]
+    return Promise.resolve(coupons)
 }
 
 export const getCouponById = (coupon_id) => {
-
-    return coupon1
+    return (coupons.find(a => a.id == coupon_id))
 }
+
+export const createCoupon = async (newCoupon) => {
+    newCoupon.id = randomBytes(12).toString('hex')
+    coupons.push(newCoupon)
+  
+    return (newCoupon)
+  }
