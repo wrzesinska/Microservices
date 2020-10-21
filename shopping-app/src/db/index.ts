d9a4c1b4a7fdc3deb479c392f3d207a98e10248a
Your Name <you@example.com>
2020-10-21 17:08:21 +0200

    Sequelize CLI - compile TS and run CLI DB commands

--- shopping-app/src/db/index.ts
+++ shopping-app/src/db/index.ts
@@ -1,9 +1,16 @@
 import { Sequelize } from "sequelize";
+// export const sequelize = new Sequelize(process.env.DB_CONNECTION) // Example for postgres
 
-export const sequelize = new Sequelize(process.env.DB_CONNECTION) // Example for postgres
+export const sequelize = new Sequelize({
+  database: process.env.DB_DATABASE,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  dialect:'postgres'
+}) // Example for postgres
 
 
-if(process.env.NODE_ENV !== 'production'){
+if (process.env.NODE_ENV !== 'production') {
   console.log('SYNCING DB STRUCTURE!!!')
   sequelize.sync()
 }
